20078bd1ab793409e72a940a534dfb6b9dcddc1a
hgbeleza <developer.beleza06@gmail.com>
2024-04-14 13:03:04 -0300

    query params

--- src/routes/cursosRoute.js
+++ src/routes/cursosRoute.js
@@ -5,10 +5,10 @@ const cursoController = new CursoController();
 
 const router = Router();
 
-router.get("/cursos", (req, res) => cursoController.pegaTodos(req, res));
+router.get("/cursos", (req, res) => cursoController.pegaCurso(req, res));
 router.get("/cursos/:id", (req, res) => cursoController.pegaUmPorId(req, res));
-// router.post("/cursos", (req, res) => cursoController.criaNovo(req, res));
-// router.put("/cursos/:id", (req, res) => cursoController.atualiza(req, res));
-// router.delete("/cursos", (req, res) => cursoController.exclui(req, res));
+router.post("/cursos", (req, res) => cursoController.criaNovo(req, res));
+router.put("/cursos/:id", (req, res) => cursoController.atualiza(req, res));
+router.delete("/cursos", (req, res) => cursoController.exclui(req, res));
 
 module.exports = router;
